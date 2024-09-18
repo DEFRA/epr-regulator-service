ccead69b8d44e1cdffd9aa6e6169fd836099c6a6
EvidenPete <155994511+EvidenPete@users.noreply.github.com>
2024-09-18 07:53:32 +0100

    initial version

---
+++ src/EPR.RegulatorService.Frontend.Web/ViewModels/Shared/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EPR.RegulatorService.Frontend.Web.ViewModels.Shared
+{
+    using EPR.RegulatorService.Frontend.Web.Extensions;
+
+    public class ErrorViewModel
+    {
+        public string Timestamp { get; set; } = DateTime.UtcNow.UtcToGmt().ToString();
+    }
+}
