87540e496690a80d51c21e66c43c6636e12da1b7
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-10-04 09:22:25 +0100

    [feature/449901-registration-submissions-page: Added an extra assert to check the back link]

--- src/EPR.RegulatorService.Frontend.UnitTests/Web/Controllers/RegistrationSubmissionsControllerTests.cs
+++ src/EPR.RegulatorService.Frontend.UnitTests/Web/Controllers/RegistrationSubmissionsControllerTests.cs
@@ -3,6 +3,7 @@ namespace EPR.RegulatorService.Frontend.UnitTests.Web.Controllers
     using EPR.RegulatorService.Frontend.Web.ViewModels.RegistrationSubmissions;
 
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
     [TestClass]
     public class RegistrationSubmissionsControllerTests : RegistrationSubmissionsTestBase
@@ -18,6 +19,7 @@ namespace EPR.RegulatorService.Frontend.UnitTests.Web.Controllers
         {
             // Arrange
             var expectedViewModel = new RegistrationSubmissionsViewModel();
+            string expectedBackLink = "/regulators/home";
 
             // Act
             var result = _controller.RegistrationSubmissions();
@@ -29,6 +31,10 @@ namespace EPR.RegulatorService.Frontend.UnitTests.Web.Controllers
             var viewResult = result as ViewResult;
             viewResult.Should().NotBeNull();
             Assert.IsInstanceOfType(expectedViewModel, viewResult.Model.GetType());
+
+            var actualBackLink = _controller.ViewBag.CustomBackLinkToDisplay;
+            Assert.IsNotNull(actualBackLink);
+            Assert.AreEqual(expectedBackLink, actualBackLink);
         }
     }
 }
