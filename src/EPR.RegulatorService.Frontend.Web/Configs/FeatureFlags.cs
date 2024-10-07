0a6ce06da747d981e160785285d270c74c301400
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-10-07 17:35:57 +0100

    [feature/452061-update-regulator-hompage: Added unit tests for the new feature flag]

--- src/EPR.RegulatorService.Frontend.Web/Configs/FeatureFlags.cs
+++ src/EPR.RegulatorService.Frontend.Web/Configs/FeatureFlags.cs
@@ -16,4 +16,6 @@ public static class FeatureFlags
     public const string PomDataPeriodAndTime = "PomDataPeriodAndTime";
 
     public const string RegistrationDataPeriodAndTime = "RegistrationDataPeriodAndTime";
+
+    public const string ManageRegistrationSubmissions = "ManageRegistrationSubmissions";
 }
