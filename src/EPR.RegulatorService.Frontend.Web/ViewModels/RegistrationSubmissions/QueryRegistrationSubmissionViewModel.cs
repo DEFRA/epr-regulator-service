19ea65014afc6c9320fa4236e37fbd6f382470b8
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-10-09 14:56:21 +0100

    [feature/449614-querying-application-registration: Refactored view model + view and created unit tests for the new GET and POST endpoints]

--- src/EPR.RegulatorService.Frontend.Web/ViewModels/RegistrationSubmissions/QueryRegistrationSubmissionViewModel.cs
+++ src/EPR.RegulatorService.Frontend.Web/ViewModels/RegistrationSubmissions/QueryRegistrationSubmissionViewModel.cs
@@ -1,10 +1,13 @@
 namespace EPR.RegulatorService.Frontend.Web.ViewModels.RegistrationSubmissions
 {
     using EPR.RegulatorService.Frontend.Web.Attributes;
+    using EPR.RegulatorService.Frontend.Web.Constants;
 
     public class QueryRegistrationSubmissionViewModel
     {
         [CharacterCount("Error.Query", "Error.QueryTooLong", 400)]
-        public string? Query { get; set; }
+        public string Query { get; set; }
+
+        public string BackToAllSubmissionsUrl { get; set; } = PagePath.RegistrationSubmissions;
     }
 }
