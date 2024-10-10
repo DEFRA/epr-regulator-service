52697b6822d436fcad3d8b10a6c82ca0d36b9e6c
Shehzad Ismail <85490459+shehzad-ismail8@users.noreply.github.com>
2024-10-10 15:39:14 +0100

    [feature/449618: Build 'Why are you rejecting this application?' page] (#64)
    * [feature/449618-rejecting-application-registration: Implemented feature as described in the ticket]
    
    * [feature/449618-rejecting-application-registration: Addressed review comment by adding additional unit tests]
    
    * [feature/449618-rejecting-application-registration: Minor refactor]

--- src/EPR.RegulatorService.Frontend.UnitTests/Web/Controllers/RegistrationSubmissionsControllerTests.cs
+++ src/EPR.RegulatorService.Frontend.UnitTests/Web/Controllers/RegistrationSubmissionsControllerTests.cs
@@ -14,6 +14,8 @@ namespace EPR.RegulatorService.Frontend.UnitTests.Web.Controllers
             SetupBase();
         }
 
+        #region RegistrationSubmissions
+
         [TestMethod]
         public async Task RegistrationSubmissions_ReturnsView_WithCorrectViewModel()
         {
@@ -36,6 +38,10 @@ namespace EPR.RegulatorService.Frontend.UnitTests.Web.Controllers
             Assert.IsNotNull(actualBackLink);
             Assert.AreEqual(expectedBackLink, actualBackLink);
         }
+
+        #endregion
+
+        #region QueryRegistrationSubmission
 
         [TestMethod]
         public async Task QueryRegistrationSubmission_ReturnsView_WithCorrectModel()
@@ -93,17 +99,214 @@ namespace EPR.RegulatorService.Frontend.UnitTests.Web.Controllers
         }
 
         [TestMethod]
-        public async Task QueryRegistrationSubmission_Post_Redirects_WhenModelStateIsValid()
+        public async Task QueryRegistrationSubmission_Post_ReturnsSuccessAndRedirectsCorrectly_WhenQueryIsValid()
         {
             // Arrange
-            var model = new QueryRegistrationSubmissionViewModel();
+            var model = new QueryRegistrationSubmissionViewModel
+            {
+                Query = "Valid query within 400 characters." // Valid input
+            };
 
             // Act
             var result = await _controller.QueryRegistrationSubmission(model) as RedirectResult;
 
             // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(PagePath.RegistrationSubmissions, result.Url);
+            Assert.IsNotNull(result); // Ensure the result is not null
+            Assert.AreEqual(PagePath.RegistrationSubmissions, result.Url); // Ensure the user is redirected to the correct URL
         }
+
+        [TestMethod]
+        public async Task QueryRegistrationSubmission_Post_ReturnsExpectedError_WhenInputIsGreaterThan400()
+        {
+            // Arrange
+            var model = new QueryRegistrationSubmissionViewModel
+            {
+                Query = new string('A', 401) // 401 characters
+            };
+
+            // Add a validation rule that the maximum length is 400 for the RejectReason property
+            _controller.ModelState.AddModelError(nameof(model.Query), "Reason for querying application must be 400 characters or less");
+
+            // Act
+            var result = await _controller.QueryRegistrationSubmission(model) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(nameof(_controller.QueryRegistrationSubmission), result.ViewName);
+            Assert.AreEqual(model, result.Model);
+
+            // Verify the error is correctly added to the model state
+            Assert.IsTrue(_controller.ModelState[nameof(model.Query)].Errors.Count > 0);
+            Assert.AreEqual("Reason for querying application must be 400 characters or less",
+                _controller.ModelState[nameof(model.Query)].Errors[0].ErrorMessage);
+
+            // Verify the back link is set correctly
+            AssertBackLink(result, $"/regulators/{PagePath.RegistrationSubmissions}");
+        }
+
+        [TestMethod]
+        public async Task QueryRegistrationSubmission_Post_ReturnsExpectedError_WhenNoQueryIsProvided()
+        {
+            // Arrange
+            var model = new QueryRegistrationSubmissionViewModel
+            {
+                Query = null // No query provided
+            };
+
+            // Simulate the model validation error for required Query field
+            _controller.ModelState.AddModelError(nameof(model.Query), "Enter the reason you are querying this registration application");
+
+            // Act
+            var result = await _controller.QueryRegistrationSubmission(model) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(nameof(_controller.QueryRegistrationSubmission), result.ViewName);
+            Assert.AreEqual(model, result.Model);
+
+            // Verify the error is correctly added to the model state
+            Assert.IsTrue(_controller.ModelState[nameof(model.Query)].Errors.Count > 0);
+            Assert.AreEqual("Enter the reason you are querying this registration application",
+                _controller.ModelState[nameof(model.Query)].Errors[0].ErrorMessage);
+
+            // Verify the back link is set correctly
+            AssertBackLink(result, $"/regulators/{PagePath.RegistrationSubmissions}");
+        }
+
+        #endregion
+
+        #region RejectRegistrationSubmission
+
+        [TestMethod]
+        public async Task RejectRegistrationSubmission_ReturnsView_WithCorrectModel()
+        {
+            // Arrange
+            string expectedBacktoAllSubmissionsUrl = PagePath.RegistrationSubmissions;
+
+            var expectedViewModel = new RejectRegistrationSubmissionViewModel
+            {
+                BackToAllSubmissionsUrl = expectedBacktoAllSubmissionsUrl
+            };
+
+            // Act
+            var result = await _controller.RejectRegistrationSubmission() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(nameof(_controller.RejectRegistrationSubmission), result.ViewName);
+            Assert.IsInstanceOfType(result.Model, typeof(RejectRegistrationSubmissionViewModel));
+            var resultViewModel = result.Model as RejectRegistrationSubmissionViewModel;
+            Assert.AreEqual(expectedViewModel.RejectReason, resultViewModel.RejectReason);
+            Assert.AreEqual(expectedViewModel.BackToAllSubmissionsUrl, resultViewModel.BackToAllSubmissionsUrl);
+        }
+
+        [TestMethod]
+        public async Task RejectRegistrationSubmission_SetsCorrectBackLinkInViewData()
+        {
+            // Act
+            var result = await _controller.RejectRegistrationSubmission() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+
+            // Check that the back link is correctly set in the ViewData
+            AssertBackLink(result, $"/regulators/{PagePath.RegistrationSubmissions}");
+        }
+
+        [TestMethod]
+        public async Task RejectRegistrationSubmission_Post_ReturnsView_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var model = new RejectRegistrationSubmissionViewModel();
+            _controller.ModelState.AddModelError("TestError", "Model state is invalid");
+
+            // Act
+            var result = await _controller.RejectRegistrationSubmission(model) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(nameof(_controller.RejectRegistrationSubmission), result.ViewName);
+            Assert.AreEqual(model, result.Model);
+
+            // Check that the back link is correctly set in the ViewData
+            AssertBackLink(result, $"/regulators/{PagePath.RegistrationSubmissions}");
+        }
+
+        [TestMethod]
+        public async Task RejectRegistrationSubmission_Post_ReturnsSuccessAndRedirectsCorrectly_WhenRejectionReasonIsValid()
+        {
+            // Arrange
+            var model = new RejectRegistrationSubmissionViewModel
+            {
+                RejectReason = "Valid rejection reason within 400 characters." // Valid input
+            };
+
+            // Act
+            var result = await _controller.RejectRegistrationSubmission(model) as RedirectResult;
+
+            // Assert
+            Assert.IsNotNull(result); // Ensure the result is not null
+            Assert.AreEqual(PagePath.RegistrationSubmissions, result.Url); // Ensure the user is redirected to the correct URL
+        }
+
+        [TestMethod]
+        public async Task RejectRegistrationSubmission_Post_ReturnsExpectedError_WhenInputIsGreaterThan400()
+        {
+            // Arrange
+            var model = new RejectRegistrationSubmissionViewModel
+            {
+                RejectReason = new string('A', 401) // 401 characters
+            };
+
+            // Add a validation rule that the maximum length is 400 for the RejectReason property
+            _controller.ModelState.AddModelError(nameof(model.RejectReason), "Reason for rejecting application must be 400 characters or less");
+
+            // Act
+            var result = await _controller.RejectRegistrationSubmission(model) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(nameof(_controller.RejectRegistrationSubmission), result.ViewName);
+            Assert.AreEqual(model, result.Model);
+
+            // Verify the error is correctly added to the model state
+            Assert.IsTrue(_controller.ModelState[nameof(model.RejectReason)].Errors.Count > 0);
+            Assert.AreEqual("Reason for rejecting application must be 400 characters or less",
+                _controller.ModelState[nameof(model.RejectReason)].Errors[0].ErrorMessage);
+
+            // Verify the back link is set correctly
+            AssertBackLink(result, $"/regulators/{PagePath.RegistrationSubmissions}");
+        }
+
+        [TestMethod]
+        public async Task RejectRegistrationSubmission_Post_ReturnsExpectedError_WhenNoRejectionReasonIsProvided()
+        {
+            // Arrange
+            var model = new RejectRegistrationSubmissionViewModel
+            {
+                RejectReason = null // No rejection reason provided
+            };
+
+            // Simulate the model validation error for required RejectionReason property
+            _controller.ModelState.AddModelError(nameof(model.RejectReason), "Enter the reason you are rejecting this registration application");
+
+            // Act
+            var result = await _controller.RejectRegistrationSubmission(model) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(nameof(_controller.RejectRegistrationSubmission), result.ViewName);
+            Assert.AreEqual(model, result.Model);
+
+            // Verify the error is correctly added to the model state
+            Assert.IsTrue(_controller.ModelState[nameof(model.RejectReason)].Errors.Count > 0);
+            Assert.AreEqual("Enter the reason you are rejecting this registration application",
+                _controller.ModelState[nameof(model.RejectReason)].Errors[0].ErrorMessage);
+
+            // Verify the back link is set correctly
+            AssertBackLink(result, $"/regulators/{PagePath.RegistrationSubmissions}");
+        }
+
+        #endregion
     }
 }
