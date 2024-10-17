e70d7fb1165145976080486a1e752501a9f25fbd
John Fitzgerald <john.fitzgerald.external@eviden.com>
2024-10-17 13:58:59 +0100

    Added ENUM for org type and status removed title from result page

--- src/EPR.RegulatorService.Frontend.Web/ViewModels/RegistrationSubmissions/RegistrationSubmissionsListViewModel.cs
+++ src/EPR.RegulatorService.Frontend.Web/ViewModels/RegistrationSubmissions/RegistrationSubmissionsListViewModel.cs
@@ -1,68 +1,82 @@
-namespace EPR.RegulatorService.Frontend.Web.ViewModels.RegistrationSubmissions
+
+using System.Diagnostics.CodeAnalysis;
+
+using EPR.RegulatorService.Frontend.Core.Enums;
+
+namespace EPR.RegulatorService.Frontend.Web.ViewModels.RegistrationSubmissions;
+
+
+[ExcludeFromCodeCoverage]
+public class RegistrationSubmissionsListViewModel
 {
-    using System.Diagnostics.CodeAnalysis;
-
-    [ExcludeFromCodeCoverage]
-    public class RegistrationSubmissionsListViewModel
+    // To Do; Data should come from the page and passed to the component
+    public RegistrationSubmissionsListViewModel()
     {
-        // To Do; Data should come from the page and passed to the component
-        public RegistrationSubmissionsListViewModel()
+        // To Do; Remove dummy data
+        PagedRegistrationSubmissionList = new List<Organisation>
         {
-            // To Do; Remove dummy data
-            PagedRegistrationSubmissionList = new List<Organisation>
-            {
-                CreateOrganisation("215 148", "A148 org Ltd", "Small producer", "11 ### ##", "2222", "GRANTED"),
-                CreateOrganisation("215 149", "A149 Ltd", "Large producer", "33 ### ##", "3333", "REFUSED"),
-                CreateOrganisation("215 150", "B150 Ltd", "Small producer", "44 ### ##", "4444", "QUERIED"),
-                CreateOrganisation("215 151", "Aceme org Ltd", "Small producer", "## ### ##", "5555", "PENDING"),
-                CreateOrganisation("215 152", "Aceme org Ltd", "Small producer", "## ### ##", "6666", "UPDATED"),
-                CreateOrganisation("215 153", "Aceme org Ltd", "Small producer", "## ### ##", "7777", "CANCELED")
-            };
-        }
-        public List<Organisation> PagedRegistrationSubmissionList { get; set; }
+            CreateOrganisation("215 151", "Aceme org Ltd", RegistrationSubmissionOrganisationType.large, "## ### ##", "5555", RegistrationSubmissionStatus.pending),
+            CreateOrganisation("215 148", "A148 org Ltd", RegistrationSubmissionOrganisationType.compliance, "11 ### ##", "2222", RegistrationSubmissionStatus.granted),
+            CreateOrganisation("215 149", "A149 Ltd", RegistrationSubmissionOrganisationType.small, "33 ### ##", "3333", RegistrationSubmissionStatus.refused),
+            CreateOrganisation("215 150", "B150 Ltd", RegistrationSubmissionOrganisationType.small, "44 ### ##", "4444", RegistrationSubmissionStatus.queried),
+            CreateOrganisation("215 152", "Aceme org Ltd", RegistrationSubmissionOrganisationType.compliance, "## ### ##", "6666", RegistrationSubmissionStatus.updated),
+            CreateOrganisation("215 153", "Aceme org Ltd", RegistrationSubmissionOrganisationType.large, "## ### ##", "7777", RegistrationSubmissionStatus.cancelled),
+            CreateOrganisation("215 148", "A148 org Ltd", RegistrationSubmissionOrganisationType.compliance, "11 ### ##", "2222", RegistrationSubmissionStatus.granted),
+            CreateOrganisation("215 149", "A149 Ltd", RegistrationSubmissionOrganisationType.small, "33 ### ##", "3333", RegistrationSubmissionStatus.refused),
+            CreateOrganisation("215 150", "B150 Ltd", RegistrationSubmissionOrganisationType.small, "44 ### ##", "4444", RegistrationSubmissionStatus.queried),
+            CreateOrganisation("215 152", "Aceme org Ltd", RegistrationSubmissionOrganisationType.compliance, "## ### ##", "6666", RegistrationSubmissionStatus.updated),
+            CreateOrganisation("215 153", "Aceme org Ltd", RegistrationSubmissionOrganisationType.large, "## ### ##", "7777", RegistrationSubmissionStatus.cancelled),
+            CreateOrganisation("215 148", "A148 org Ltd", RegistrationSubmissionOrganisationType.compliance, "11 ### ##", "2222", RegistrationSubmissionStatus.granted),
+            CreateOrganisation("215 149", "A149 Ltd", RegistrationSubmissionOrganisationType.small, "33 ### ##", "3333", RegistrationSubmissionStatus.refused),
+            CreateOrganisation("215 150", "B150 Ltd", RegistrationSubmissionOrganisationType.small, "44 ### ##", "4444", RegistrationSubmissionStatus.queried),
+            CreateOrganisation("215 152", "Aceme org Ltd", RegistrationSubmissionOrganisationType.compliance, "## ### ##", "6666", RegistrationSubmissionStatus.updated),
+            CreateOrganisation("215 153", "Aceme org Ltd", RegistrationSubmissionOrganisationType.large, "## ### ##", "7777", RegistrationSubmissionStatus.cancelled)
+        };
+    }
+    public List<Organisation> PagedRegistrationSubmissionList { get; set; }
 
-        private Organisation CreateOrganisation(string id, string name, string type, string refNumber, string year, string status)
+    private Organisation CreateOrganisation(string id, string name, RegistrationSubmissionOrganisationType type, string refNumber, string year, RegistrationSubmissionStatus status)
+    {
+        return new Organisation
         {
-            return new Organisation
-            {
-                OrganisationId = id,
-                OrganisationName = name,
-                ProducerType = type,
-                RefNumber = refNumber,
-                SubmissionDateTime = DateTime.Now,
-                SubmissionYear = year,
-                Status = status
-            };
-        }
-
-        // To Do; Use ENUMS from common data instead of strings here
-        public string GetStyleNameFromStatus(string status)
-        {
-            return status switch
-            {
-                "GRANTED" => "govuk-tag--green",
-                "REFUSED" => "govuk-tag--red",
-                "QUERIED" => "govuk-tag--purple",
-                "PENDING" => "govuk-tag--blue",
-                "UPDATED" => "govuk-tag--yellow",
-                "CANCELED" => "govuk-tag--grey",
-                _ => "govuk-tag--purple"
-            };
-
-        }
+            OrganisationId = id,
+            OrganisationName = name,
+            ProducerType = type,
+            RefNumber = refNumber,
+            SubmissionDateTime = DateTime.Now,
+            SubmissionYear = year,
+            Status = status
+        };
     }
 
-    // To Do; Replace with actual data model class. Remove dummy class
-    [ExcludeFromCodeCoverage]
-    public class Organisation
+    // To Do; Use ENUMS from common data instead of strings here
+    public string GetStyleNameFromStatus(RegistrationSubmissionStatus status )
     {
-        public string OrganisationName { get; set; }
-        public string OrganisationId { get; set; }
-        public string ProducerType { get; set; }
-        public string RefNumber { get; set; }
-        public DateTime SubmissionDateTime { get; set; }
-        public string SubmissionYear { get; set; }
-        public string Status { get; set; }
+        return status switch
+        {
+            RegistrationSubmissionStatus.granted => "govuk-tag--green",
+            RegistrationSubmissionStatus.refused => "govuk-tag--red",
+            RegistrationSubmissionStatus.queried => "govuk-tag--purple",
+            RegistrationSubmissionStatus.pending => "govuk-tag--blue",
+            RegistrationSubmissionStatus.updated => "govuk-tag--yellow",
+            RegistrationSubmissionStatus.cancelled => "govuk-tag--grey",
+            RegistrationSubmissionStatus.none => "govuk-tag--purple",
+            _ => "govuk-tag--purple"
+        };
+
     }
+}
+
+// To Do; Replace with actual data model class. Remove dummy class
+[ExcludeFromCodeCoverage]
+public class Organisation
+{
+    public string OrganisationName { get; set; }
+    public string OrganisationId { get; set; }
+    public RegistrationSubmissionOrganisationType ProducerType { get; set; }
+    public string RefNumber { get; set; }
+    public DateTime SubmissionDateTime { get; set; }
+    public string SubmissionYear { get; set; }
+    public RegistrationSubmissionStatus Status { get; set; }
 }
 
