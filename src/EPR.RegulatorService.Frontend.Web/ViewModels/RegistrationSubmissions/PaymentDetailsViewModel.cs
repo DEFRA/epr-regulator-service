2875e35f5d6fbfddcb35c6720b207bacd4474fc5
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-10-31 12:32:43 +0000

    [feature/461183-submit-offline-payment-confirmation: Addressed SonarCube issue]

--- src/EPR.RegulatorService.Frontend.Web/ViewModels/RegistrationSubmissions/PaymentDetailsViewModel.cs
+++ src/EPR.RegulatorService.Frontend.Web/ViewModels/RegistrationSubmissions/PaymentDetailsViewModel.cs
@@ -64,7 +64,7 @@ namespace EPR.RegulatorService.Frontend.Web.ViewModels.RegistrationSubmissions
             OnlineMarketplaceFee = details.OnlineMarketplaceFee,
             PreviousPaymentsReceived = details.PreviousPaymentsReceived,
             SubsidiaryFee = details.SubsidiaryFee,
-            OfflinePayment = details.OfflinePaymentAmount.ToString()
+            OfflinePayment = string.Format(CultureInfo.InvariantCulture, "{0:F2}", details.OfflinePaymentAmount)
         };
     }
 }
