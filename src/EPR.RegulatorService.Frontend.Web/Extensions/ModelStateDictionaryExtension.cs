07b4e7dd056241a867ae4e637e30aabdcafbbafa
prasad.ramireddy@defra.onmicrosoft.com <prasad.ramireddy@defra.onmicrosoft.com>
2024-11-15 16:37:20 +0000

    Added code for Cancellation field validation

--- src/EPR.RegulatorService.Frontend.Web/Extensions/ModelStateDictionaryExtension.cs
+++ src/EPR.RegulatorService.Frontend.Web/Extensions/ModelStateDictionaryExtension.cs
@@ -1,4 +1,5 @@
-﻿using EPR.RegulatorService.Frontend.Web.ViewModels.Shared.GovUK;
+using EPR.RegulatorService.Frontend.Web.ViewModels.Shared.GovUK;
+
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace EPR.RegulatorService.Frontend.Web.Extensions;
@@ -26,12 +27,45 @@ public static class ModelStateDictionaryExtension
         var groupedErrors = errors
             .GroupBy(e => e.Key)
             .OrderBy(e => e.Key);
-        
+
         foreach (var error in groupedErrors)
         {
             errorsDictionary.Add((error.Key, error.ToList()));
         }
-        
+
+        return errorsDictionary;
+    }
+
+    public static List<(string Key, List<ErrorViewModel> Errors)> ToErrorDictionaryByKey(this ModelStateDictionary modelState, string key)
+    {
+        var errors = new List<ErrorViewModel>();
+
+        foreach (var item in modelState)
+        {
+            if (item.Key == key)
+            {
+                foreach (var error in item.Value.Errors)
+                {
+                    errors.Add(new ErrorViewModel
+                    {
+                        Key = item.Key,
+                        Message = error.ErrorMessage
+                    });
+                }
+            }
+        }
+
+        var errorsDictionary = new List<(string Key, List<ErrorViewModel> Errors)>();
+
+        var groupedErrors = errors
+            .GroupBy(e => e.Key)
+            .OrderBy(e => e.Key);
+
+        foreach (var error in groupedErrors)
+        {
+            errorsDictionary.Add((error.Key, error.ToList()));
+        }
+
         return errorsDictionary;
     }
 }
