3f632bc7a14de3648f2c60ce3b5e3121f1eb17c1
Maksim Varkus <maksim.varkus@eviden.com>
2024-12-19 16:16:20 +0000

    Add numbers to enum

--- src/EPR.RegulatorService.Frontend.Core/Enums/OrganisationSubmissionStatus.cs
+++ src/EPR.RegulatorService.Frontend.Core/Enums/OrganisationSubmissionStatus.cs
@@ -2,7 +2,7 @@ namespace EPR.RegulatorService.Frontend.Core.Enums;
 
 public enum OrganisationSubmissionStatus
 {
-    Pending,
-    Accepted,
-    Rejected
+    Pending = 1,
+    Accepted = 2,
+    Rejected = 3
 }
