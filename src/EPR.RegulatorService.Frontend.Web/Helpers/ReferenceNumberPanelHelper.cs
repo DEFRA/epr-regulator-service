b5cc21b9712cd72cc198ee61e63cbd5b115be634
Shehzad Ismail <shehzad.ismail@eviden.com>
2025-02-12 14:43:57 +0000

    [feature/496625-update-organisation-details-page-resubmission: WIP extending ReferenceNumberPanelTagHelper]

--- src/EPR.RegulatorService.Frontend.Web/Helpers/ReferenceNumberPanelHelper.cs
+++ src/EPR.RegulatorService.Frontend.Web/Helpers/ReferenceNumberPanelHelper.cs
@@ -1,15 +1,58 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Localization;
+using EPR.RegulatorService.Frontend.Web.ViewModels.RegistrationSubmissions;
+
 namespace EPR.RegulatorService.Frontend.Web.Helpers
 {
-    using EPR.RegulatorService.Frontend.Web.ViewModels.RegistrationSubmissions;
-
-    public static class ReferenceNumberPanelHelper
+    [HtmlTargetElement("reference-number-panel", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class ReferenceNumberPanelTagHelper : TagHelper
     {
-        public static string GetPanelTitle(RegistrationSubmissionDetailsViewModel model) => !string.IsNullOrWhiteSpace(model.RegistrationReferenceNumber)
-                ? "RegistrationSubmissionDetails.RegistrationReferenceNumber"
-                : "RegistrationSubmissionDetails.ApplicationReferenceNumber";
+        private const string ModelAttributeName = "model";
 
-        public static string GetPanelContent(RegistrationSubmissionDetailsViewModel model) => !string.IsNullOrWhiteSpace(model.RegistrationReferenceNumber)
-            ? model.RegistrationReferenceNumber
-            : model.ReferenceNumber;
+        private readonly IStringLocalizer _localizer;
+
+        public ReferenceNumberPanelTagHelper(IStringLocalizerFactory localizerFactory)
+        {
+            var type = typeof(SharedResources);
+            _localizer = localizerFactory.Create(type);
+        }
+
+        [HtmlAttributeName(ModelAttributeName)]
+        public RegistrationSubmissionDetailsViewModel Model { get; set; } = null!;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.TagName = "div";
+            output.AddClass("info-panel", HtmlEncoder.Default);
+            output.AddClass("info-panel__granted", HtmlEncoder.Default);
+
+            var contentBuilder = new System.Text.StringBuilder();
+
+            if (Model.IsResubmission)
+            {
+                contentBuilder.AppendLine($"<h3>{_localizer["RegistrationSubmissionDetails.RegistrationReferenceNumber"].Value}</h3>");
+                contentBuilder.AppendLine($"<p>{Model.RegistrationReferenceNumber}</p>");
+                contentBuilder.AppendLine($"<h3>{_localizer["RegistrationSubmissionDetails.ApplicationReferenceNumber"].Value}</h3>");
+                contentBuilder.AppendLine($"<p>{Model.ReferenceNumber}</p>");
+            }
+            else
+            {
+                string titleKey = !string.IsNullOrWhiteSpace(Model.RegistrationReferenceNumber)
+                    ? _localizer["RegistrationSubmissionDetails.RegistrationReferenceNumber"]
+                    : _localizer["RegistrationSubmissionDetails.ApplicationReferenceNumber"];
+
+                string referenceNumber = !string.IsNullOrWhiteSpace(Model.RegistrationReferenceNumber)
+                    ? Model.RegistrationReferenceNumber
+                    : Model.ReferenceNumber;
+
+                contentBuilder.AppendLine($"<h3>{_localizer[titleKey].Value}</h3>");
+                contentBuilder.AppendLine($"<p>{referenceNumber}</p>");
+            }
+
+            output.Content.SetHtmlContent(contentBuilder.ToString());
+        }
     }
 }
