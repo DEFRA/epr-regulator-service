991219591d549d2b16dbd37e1cdb44c079774598
Shehzad Ismail <shehzad.ismail@eviden.com>
2025-02-12 17:31:20 +0000

    [feature/496625-update-organisation-details-page-resubmission: Add more unit tests to ReferenceNumberPanelHelperTests]

--- src/EPR.RegulatorService.Frontend.UnitTests/Web/Helpers/ReferenceNumberPanelHelperTests.cs
+++ src/EPR.RegulatorService.Frontend.UnitTests/Web/Helpers/ReferenceNumberPanelHelperTests.cs
@@ -1,5 +1,7 @@
 namespace EPR.RegulatorService.Frontend.UnitTests.Web.Helpers
 {
+    using System.Text;
+
     using EPR.RegulatorService.Frontend.Core.Enums;
     using EPR.RegulatorService.Frontend.Web.Helpers;
     using EPR.RegulatorService.Frontend.Web.ViewModels.RegistrationSubmissions;
@@ -38,41 +40,45 @@ namespace EPR.RegulatorService.Frontend.UnitTests.Web.Helpers
         [TestMethod]
         public void GetPanelTitle_WhenStatusIsGranted_ReturnsRegistrationReferenceNumber()
         {
-            // Arrange
             var model = new RegistrationSubmissionDetailsViewModel
             {
                 RegistrationReferenceNumber = "REG123",
                 Status = RegistrationSubmissionStatus.None
             };
 
-            // Act
             string? result = ReferenceNumberPanelHelper.GetPanelTitle(model);
-
-            // Assert
             Assert.AreEqual("RegistrationSubmissionDetails.RegistrationReferenceNumber", result);
+        }
+
+        [TestMethod]
+        public void GetPanelTitle_WhenStatusIsGrantedAndIsResubmission_ReturnsBothTitles()
+        {
+            var model = new RegistrationSubmissionDetailsViewModel
+            {
+                Status = RegistrationSubmissionStatus.Granted,
+                IsResubmission = true
+            };
+
+            string? result = ReferenceNumberPanelHelper.GetPanelTitle(model);
+            Assert.AreEqual("RegistrationSubmissionDetails.RegistrationReferenceNumber|RegistrationSubmissionDetails.ApplicationReferenceNumber", result);
         }
 
         [TestMethod]
         public void GetPanelContent_When_RegNumberSupplied_ReturnsRegistrationReferenceNumber()
         {
-            // Arrange
             var model = new RegistrationSubmissionDetailsViewModel
             {
                 Status = RegistrationSubmissionStatus.Granted,
                 RegistrationReferenceNumber = "REG123"
             };
 
-            // Act
             string? result = ReferenceNumberPanelHelper.GetPanelContent(model);
-
-            // Assert
             Assert.AreEqual("REG123", result);
         }
 
         [TestMethod]
         public void GetPanelContent_ReturnsRegistrationReferenceNumber_EvenWhenThereIsAnAppRefNum()
         {
-            // Arrange
             var model = new RegistrationSubmissionDetailsViewModel
             {
                 Status = RegistrationSubmissionStatus.Granted,
@@ -80,28 +86,41 @@ namespace EPR.RegulatorService.Frontend.UnitTests.Web.Helpers
                 RegistrationReferenceNumber = "REG123"
             };
 
-            // Act
             string? result = ReferenceNumberPanelHelper.GetPanelContent(model);
-
-            // Assert
             Assert.AreEqual("REG123", result);
         }
 
         [TestMethod]
         public void GetPanelContent_When_NoRegNumberSupplied_ReturnsApplicationReferenceNumber()
         {
-            // Arrange
             var model = new RegistrationSubmissionDetailsViewModel
             {
                 Status = RegistrationSubmissionStatus.Pending,
                 ReferenceNumber = "APP456"
             };
 
-            // Act
             string? result = ReferenceNumberPanelHelper.GetPanelContent(model);
-
-            // Assert
             Assert.AreEqual("APP456", result);
+        }
+
+        [TestMethod]
+        public void GetPanelContent_WhenStatusIsGrantedAndIsResubmission_ReturnsBothNumbers()
+        {
+            var model = new RegistrationSubmissionDetailsViewModel
+            {
+                Status = RegistrationSubmissionStatus.Granted,
+                IsResubmission = true,
+                RegistrationReferenceNumber = "REG123",
+                ReferenceNumber = "APP456"
+            };
+
+            string? result = ReferenceNumberPanelHelper.GetPanelContent(model);
+            string expected = new StringBuilder()
+                .AppendLine("REG123")
+                .AppendLine("APP456")
+                .ToString();
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
