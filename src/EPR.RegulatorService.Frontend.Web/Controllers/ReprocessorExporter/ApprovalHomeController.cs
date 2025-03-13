15451e30679d9b33bcdca66ddcc99251c3d050c4
Paul Shimmen <paul.shimmen@eviden.com>
2025-03-13 08:54:40 +0000

    Changed page name to manage-approvals

--- src/EPR.RegulatorService.Frontend.Web/Controllers/ReprocessorExporter/ApprovalHomeController.cs
+++ src/EPR.RegulatorService.Frontend.Web/Controllers/ReprocessorExporter/ApprovalHomeController.cs
@@ -18,27 +18,19 @@ namespace EPR.RegulatorService.Frontend.Web.Controllers.ReprocessorExporter
 {
     [FeatureGate(FeatureFlags.ReprocessorExporter)]
     [Authorize(Policy = PolicyConstants.RegulatorBasicPolicy)] // PAUL -- need to create new policy going forward
-    // Define multiple routes for different combinations
-    [Route(PagePath.ReprocessorExporterHome)] // manage-registrations-reprocessor
-    [Route(PagePath.ReprocessorExporterAccreditation)] // manage-accreditation-reprocessor
-    [Route(PagePath.ExporterRegistrationHome)] // manage-registrations-exporter
-    [Route(PagePath.ExporterAccreditationHome)] // manage-accreditation-exporter
+    [Route(PagePath.ReprocessorExporterHome)]
     public class ApprovalHomeController : Controller
     {
         [HttpGet]
         public IActionResult Index()
         {
-            // Determine organisation type and registration/accreditation dynamically
-            var isRegistration = Request.Path.Value.Contains("registrations");
-            var isExporter = Request.Path.Value.Contains("exporter");
-
             var model = new ReprocessorExporterHomeViewModel
             {
                 OrganisationName = "Green Ltd",
                 SiteAddress = "23 Ruby St, London, E12 3SE",
-                OrganisationType = isExporter ? "Exporter" : "Reprocessor",
+                OrganisationType = "Exporter",
                 Regulator = "Environment Agency (EA)",
-                IsRegistration = isRegistration
+                IsRegistration = true
             };
 
             return View("~/Views/ReprocessorExporter/ApprovalHome/Index.cshtml", model);
