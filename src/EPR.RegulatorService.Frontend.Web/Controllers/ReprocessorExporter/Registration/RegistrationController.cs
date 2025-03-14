425785e5797f20bb41b785e07717527c046b90a0
Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
2025-03-14 15:31:58 +0000

    feature/515162 Reprocessors - Site address - View UI only

---
+++ src/EPR.RegulatorService.Frontend.Web/Controllers/ReprocessorExporter/Registration/RegistrationController.cs
@@ -0,0 +1,29 @@
+using EPR.RegulatorService.Frontend.Core.Enums;
+using EPR.RegulatorService.Frontend.Web.Configs;
+using EPR.RegulatorService.Frontend.Web.Constants;
+using EPR.RegulatorService.Frontend.Web.ViewModels.ReprocessorExporter;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace EPR.RegulatorService.Frontend.Web.Controllers.ReprocessorExporter.Registration;
+
+[FeatureGate(FeatureFlags.ReprocessorExporter)]
+[Route(PagePath.ReprocessorExporterRegistrations)]
+public class RegistrationController : Controller
+{
+    [HttpGet]
+    [Route(PagePath.UkSiteDetails)]
+    public IActionResult UkSiteDetails()
+    {
+        ViewBag.BackLinkToDisplay = PagePath.ManageRegistrations;
+
+        var model = new ManageRegistrationsViewModel
+        {
+            ApplicationOrganisationType = ApplicationOrganisationType.Reprocessor
+        };
+
+        return View("~/Views/ReprocessorExporter/Reprocessor/UkSiteDetails.cshtml", model);
+
+    }
+}
