de99c1a0fe0a0935fa9e5dab01b5e6aef55a1557
Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
2025-03-17 15:57:30 +0000

    session journey

--- src/EPR.RegulatorService.Frontend.Web/Controllers/ReprocessorExporter/Registration/RegistrationController.cs
+++ src/EPR.RegulatorService.Frontend.Web/Controllers/ReprocessorExporter/Registration/RegistrationController.cs
@@ -1,9 +1,12 @@
 using EPR.RegulatorService.Frontend.Core.Enums;
+using EPR.RegulatorService.Frontend.Core.Sessions;
 using EPR.RegulatorService.Frontend.Web.Configs;
 using EPR.RegulatorService.Frontend.Web.Constants;
+using EPR.RegulatorService.Frontend.Web.Sessions;
 using EPR.RegulatorService.Frontend.Web.ViewModels.ReprocessorExporter;
 
 using Microsoft.AspNetCore.Mvc;
+using EPR.RegulatorService.Frontend.Core.Extensions;
 using Microsoft.FeatureManagement.Mvc;
 
 namespace EPR.RegulatorService.Frontend.Web.Controllers.ReprocessorExporter.Registration;
@@ -12,13 +15,22 @@ namespace EPR.RegulatorService.Frontend.Web.Controllers.ReprocessorExporter.Regi
 [Route(PagePath.ReprocessorExporterRegistrations)]
 public class RegistrationController : Controller
 {
+    private readonly ISessionManager<JourneySession> _sessionManager;
+
+    public RegistrationController(
+        ISessionManager<JourneySession> sessionManager)
+    {
+        _sessionManager = sessionManager;
+    }
+
     [HttpGet]
     [Route(PagePath.UkSiteDetails)]
-    public IActionResult UkSiteDetails()
+    public async Task<IActionResult> UkSiteDetails()
     {
-        ViewBag.BackLinkToDisplay = PagePath.ManageRegistrations;
-
-        ViewBag.BackLinkAriaLabel = "Click here if you wish to go back to the previous page";//will be added to localizer
+        var session = await _sessionManager.GetSessionAsync(HttpContext.Session);
+        session.ReprocessorExporterSession.Journey.AddIfNotExists(PagePath.ManageRegistrations);
+        SaveSessionAndJourney(session, PagePath.ManageRegistrations, PagePath.UkSiteDetails);
+        SetBackLink(session, PagePath.UkSiteDetails);
 
         var model = new ManageRegistrationsViewModel
         {
@@ -26,5 +38,32 @@ public class RegistrationController : Controller
         };
 
         return View("~/Views/ReprocessorExporter/Reprocessor/UkSiteDetails.cshtml", model);
+    }
+
+    private async Task SaveSessionAndJourney(JourneySession session, string sourcePagePath, string? destinationPagePath)
+    {
+        ClearRestOfJourney(session, sourcePagePath);
+
+        session.ReprocessorExporterSession.Journey.AddIfNotExists(destinationPagePath);
+
+        await SaveSession(session);
+    }
+
+    private async Task SaveSession(JourneySession session)
+    {
+        await _sessionManager.SaveSessionAsync(HttpContext.Session, session);
+    }
+
+    private static void ClearRestOfJourney(JourneySession session, string currentPagePath)
+    {
+        var index = session.ReprocessorExporterSession.Journey.IndexOf(currentPagePath);
+
+        // this also cover if current page not found (index = -1) then it clears all pages
+        session.ReprocessorExporterSession.Journey = session.ReprocessorExporterSession.Journey.Take(index + 1).ToList();
+    }
+    private void SetBackLink(JourneySession session, string currentPagePath)
+    {
+        ViewBag.BackLinkToDisplay = session.ReprocessorExporterSession.Journey.PreviousOrDefault(currentPagePath) ?? string.Empty;
+        ViewBag.BackLinkAriaLabel = "Click here if you wish to go back to the previous page";//will be added to localizer
     }
 }
