bc9a8bdd9c3b662bf88503a57513a7298db510d3
Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
2025-03-19 11:01:41 +0000

    feature/515229 Reprocessors - Sampling and inspection plan

--- src/EPR.RegulatorService.Frontend.Web/Constants/PagePath.cs
+++ src/EPR.RegulatorService.Frontend.Web/Constants/PagePath.cs
@@ -60,6 +60,7 @@ public static class PagePath
     public const string ManageRegistrations = "manage-registrations";
     public const string Accreditation = "accreditation";
     public const string UkSiteDetails="uksite-details";
+    public const string SamplingInspection = "sampling-inspection";
     // Non journey paths
     public const string Accessibility = "accessibility";
     public const string SignedOut = "signed-out";
