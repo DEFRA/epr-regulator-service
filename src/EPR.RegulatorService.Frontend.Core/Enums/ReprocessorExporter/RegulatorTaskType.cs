a507df377543f8cb45172967d0ee198509135d66
Tim Jones <tim.jones.external@eviden.com>
2025-04-04 16:55:55 +0100

    Removed unused code.

--- src/EPR.RegulatorService.Frontend.Core/Enums/ReprocessorExporter/RegulatorTaskType.cs
+++ src/EPR.RegulatorService.Frontend.Core/Enums/ReprocessorExporter/RegulatorTaskType.cs
@@ -2,8 +2,6 @@ namespace EPR.RegulatorService.Frontend.Core.Enums.ReprocessorExporter;
 
 public enum RegulatorTaskType
 {
-    // TODO: Need to confirm these names match the database values
-
     BusinessAddress,
     MaterialsAuthorisedOnSite,
     MaterialDetailsAndContact,
