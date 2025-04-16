068d25fee0ebc690237043e823ffcfe86807d5b5
Tim Jones <tim.jones.external@eviden.com>
2025-04-16 11:03:45 +0100

    Removed commented code.

--- src/EPR.RegulatorService.Frontend.Web/Controllers/ReprocessorExporter/Registrations/RegistrationsController.cs
+++ src/EPR.RegulatorService.Frontend.Web/Controllers/ReprocessorExporter/Registrations/RegistrationsController.cs
@@ -10,8 +10,6 @@ using Microsoft.FeatureManagement.Mvc;
 
 namespace EPR.RegulatorService.Frontend.Web.Controllers.ReprocessorExporter.Registrations;
 
-using Core.Enums.ReprocessorExporter;
-
 [FeatureGate(FeatureFlags.ReprocessorExporter)]
 [Authorize(Policy = PolicyConstants.RegulatorBasicPolicy)]
 [Route(PagePath.ReprocessorExporterRegistrations)]
@@ -24,7 +22,6 @@ public class RegistrationsController(
     [Route(PagePath.AuthorisedMaterials)]
     public async Task<IActionResult> AuthorisedMaterials(int registrationId)
     {
-        // TaskName = RegulatorTaskType.MaterialsAuthorisedOnSite
         var session = await GetSession();
 
         await SaveSessionAndJourney(session, PagePath.AuthorisedMaterials);
@@ -37,7 +34,6 @@ public class RegistrationsController(
     [Route(PagePath.UkSiteDetails)]
     public async Task<IActionResult> UkSiteDetails(int registrationId)
     {
-        // TaskName = RegulatorTaskType.SiteAddressAndContactDetails
         var session = await GetSession();
 
         await SaveSessionAndJourney(session, PagePath.UkSiteDetails);
@@ -51,7 +47,6 @@ public class RegistrationsController(
     [Route(PagePath.MaterialWasteLicences)]
     public async Task<IActionResult> MaterialWasteLicences(int registrationMaterialId)
     {
-        //TaskName = RegulatorTaskType.WasteLicensesPermitsAndExemptions
         var session = await GetSession();
 
         await SaveSessionAndJourney(session, PagePath.MaterialWasteLicences);
@@ -64,7 +59,6 @@ public class RegistrationsController(
     [Route(PagePath.SamplingInspection)]
     public async Task<IActionResult> SamplingInspection(int registrationMaterialId)
     {
-        //TaskName = RegulatorTaskType.SamplingAndInspectionPlan
         var session = await GetSession();
 
         await SaveSessionAndJourney(session, PagePath.SamplingInspection);
@@ -77,7 +71,6 @@ public class RegistrationsController(
     [Route(PagePath.InputsAndOutputs)]
     public async Task<IActionResult> InputsAndOutputs(int registrationMaterialId)
     {
-        //TaskName = RegulatorTaskType.ReprocessingInputsAndOutputs
         var session = await GetSession();
 
         await SaveSessionAndJourney(session, PagePath.InputsAndOutputs);
@@ -90,7 +83,6 @@ public class RegistrationsController(
     [Route(PagePath.WasteLicences)]
     public async Task<IActionResult> WasteLicences(int registrationId)
     {
-        //TaskName = RegulatorTaskType.WasteLicensesPermitsAndExemptions
         var session = await GetSession();
 
         await SaveSessionAndJourney(session, PagePath.WasteLicences);
@@ -103,7 +95,6 @@ public class RegistrationsController(
     [Route(PagePath.BusinessAddress)]
     public async Task<IActionResult> BusinessAddress(int registrationId)
     {
-        //TaskName = RegulatorTaskType.BusinessAddress
         var session = await GetSession();
 
         await SaveSessionAndJourney(session, PagePath.BusinessAddress);
@@ -116,7 +107,6 @@ public class RegistrationsController(
     [Route(PagePath.MaterialDetails)]
     public async Task<IActionResult> MaterialDetails(int registrationMaterialId)
     {
-        //TaskName = RegulatorTaskType.MaterialDetailsAndContact
         var session = await GetSession();
 
         await SaveSessionAndJourney(session, PagePath.MaterialDetails);
@@ -129,7 +119,6 @@ public class RegistrationsController(
     [Route(PagePath.OverseasReprocessorInterim)]
     public async Task<IActionResult> OverseasReprocessorInterim(int registrationMaterialId)
     {
-        //TaskName = RegulatorTaskType.OverseasReprocessorAndInterimSiteDetails
         var session = await GetSession();
 
         await SaveSessionAndJourney(session, PagePath.OverseasReprocessorInterim);
