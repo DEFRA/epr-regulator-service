d8fd872a2eddb6fc9e3ff2f37514f757f3bae7c7
Tim Jones <tim.jones.external@eviden.com>
2025-05-01 15:54:25 +0100

    Update permit type text.

--- src/EPR.RegulatorService.Frontend.Web/Constants/ReprocessorExporter/PermitTypes.cs
+++ src/EPR.RegulatorService.Frontend.Web/Constants/ReprocessorExporter/PermitTypes.cs
@@ -5,9 +5,8 @@ using System.Diagnostics.CodeAnalysis;
 [ExcludeFromCodeCoverage]
 public static class PermitTypes
 {
-    // TODO: These spellings are incorrect ("Contril", "License") but need to be corrected in the ADR, Facade and BE before changing here.
     public const string WasteExemption = "Waste Exemption";
-    public const string PollutionPreventionAndControlPermit = "Pollution,Prevention and Contril(PPC) permit";
+    public const string PollutionPreventionAndControlPermit = "Pollution,Prevention and Control(PPC) permit";
     public const string WasteManagementLicence = "Waste Management License";
     public const string InstallationPermit = "Installation Permit";
     public const string EnvironmentalPermitOrWasteManagementLicence = "Environmental permit or waste management license";
