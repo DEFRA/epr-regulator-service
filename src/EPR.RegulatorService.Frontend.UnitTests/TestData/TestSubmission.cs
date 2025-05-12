1386831e95aadd3b1d0de1f304d6aee40699c976
Julian Young <julian.young.external@eviden.com>
2025-05-12 17:32:27 +0100

    Bug/547592 multiple tab unsuccessful offline payment (#280)
    * All required changes brought to RC10
    
    * Corrected SubmissionController Tests
    
    * Altered to ensure the PR contains the right code !(????)!

--- src/EPR.RegulatorService.Frontend.UnitTests/TestData/TestSubmission.cs
+++ src/EPR.RegulatorService.Frontend.UnitTests/TestData/TestSubmission.cs
@@ -29,5 +29,28 @@ namespace EPR.RegulatorService.Frontend.UnitTests.TestData
                 PomBlobName = "A blob name",
                 PomFileName = "A Pom File.csv"
             };
+
+        public static Submission GetTestSubmission(string prefix) =>
+            new()
+            {
+                SubmissionId = Guid.NewGuid(),
+                SubmittedDate = new DateTime(2023, 03, 03, 03, 03, 33, DateTimeKind.Unspecified),
+                Decision = SubmissionStatus.Pending,
+                IsResubmission = false,
+                IsResubmissionRequired = false,
+                Comments = string.Empty,
+                OrganisationId = Guid.NewGuid(),
+                OrganisationName = $"{prefix} Test Org Ltd.",
+                OrganisationType = OrganisationType.DirectProducer,
+                OrganisationReference = "123 456",
+                Email = $"{prefix}test@abc.com",
+                UserId = Guid.NewGuid(),
+                FirstName = $"{prefix} Test",
+                LastName = $"{prefix} User",
+                Telephone = "0123 456 789",
+                ServiceRole = "Approved person",
+                PomBlobName = "A blob name",
+                PomFileName = $"{prefix} A Pom File.csv"
+            };
     }
 }
