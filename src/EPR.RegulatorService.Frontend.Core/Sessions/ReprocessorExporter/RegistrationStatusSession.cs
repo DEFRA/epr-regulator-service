ae9ac18a94ae9febe844742ac0a196e3dd080d12
Tim Jones <tim.jones.external@eviden.com>
2025-05-14 09:18:14 +0100

    Hide site address for exporters in header of Check Registration sub-pages.

--- src/EPR.RegulatorService.Frontend.Core/Sessions/ReprocessorExporter/RegistrationStatusSession.cs
+++ src/EPR.RegulatorService.Frontend.Core/Sessions/ReprocessorExporter/RegistrationStatusSession.cs
@@ -6,6 +6,7 @@ public class RegistrationStatusSession
 {
     public int RegistrationId { get; set; }
     public required string OrganisationName { get; init; }
+    public ApplicationOrganisationType ApplicationType { get; init; }
     public required string SiteAddress { get; init; }
     public required int RegistrationMaterialId { get; init; }
     public required string MaterialName { get; init; }
