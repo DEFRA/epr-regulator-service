aadf23206fc3f7912a0189cd586c35abed96e9d2
Paul Shimmen <paul.shimmen@eviden.com>
2025-05-23 12:56:39 +0100

    Adding more tests

--- src/EPR.RegulatorService.Frontend.Web/Mappings/ManageAccreditationsMappingProfile.cs
+++ src/EPR.RegulatorService.Frontend.Web/Mappings/ManageAccreditationsMappingProfile.cs
@@ -48,38 +48,4 @@ public class ManageAccreditationsMappingProfile : Profile
             .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
             .ForMember(dest => dest.Year, opt => opt.Ignore());
     }
-
-    private static string MapRegistrationMaterialStatusCssClass(ApplicationStatus? status) =>
-        status switch
-        {
-            ApplicationStatus.Granted => "govuk-tag--green",
-            ApplicationStatus.Refused => "govuk-tag--red",
-            _ => "govuk-tag--grey"
-        };
-
-    private static string MapRegistrationMaterialStatusText(ApplicationStatus? status)
-    {
-        if (status == null)
-        {
-            return "Not started yet";
-        }
-
-        return status.ToString()!;
-    }
-
-    private static string MapRegistrationTaskStatusCssClass(RegulatorTaskStatus taskStatus) =>
-        taskStatus switch
-        {
-            RegulatorTaskStatus.Queried => "govuk-tag--orange",
-            RegulatorTaskStatus.Completed => "govuk-tag--blue",
-            _ => "govuk-tag--grey"
-        };
-
-    private static string MapRegistrationTaskStatusText(RegulatorTaskStatus taskStatus) =>
-        taskStatus switch
-        {
-            RegulatorTaskStatus.NotStarted => "Not started yet",
-            RegulatorTaskStatus.Completed => "Reviewed",
-            _ => taskStatus.ToString()
-        };
 }
