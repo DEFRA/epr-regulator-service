72a5e72acae69bf981a9d141db5b30d821dbdcf3
Bhaarath Balraj <bhaarath.balrak@Defra.onmicrosoft.com>
2025-06-10 16:49:40 +0100

    Fixes for Check accreditation status happy path issues

--- src/EPR.RegulatorService.Frontend.Web/ViewModels/ReprocessorExporter/Registrations/RegistrationStatus/RegistrationStatusViewModelBase.cs
+++ src/EPR.RegulatorService.Frontend.Web/ViewModels/ReprocessorExporter/Registrations/RegistrationStatus/RegistrationStatusViewModelBase.cs
@@ -2,9 +2,16 @@ using EPR.RegulatorService.Frontend.Core.Enums.ReprocessorExporter;
 
 namespace EPR.RegulatorService.Frontend.Web.ViewModels.ReprocessorExporter.Registrations.RegistrationStatus;
 
+using System.ComponentModel.DataAnnotations;
+
 public abstract class RegistrationStatusViewModelBase
 {
     public string? OrganisationName { get; init; }
     public string? SiteAddress { get; init; }
     public required ApplicationOrganisationType? ApplicationType { get; init; }
+    public int? Day { get; init; }
+    public int? Month { get; init; }
+
+    [Range(2000, 2100, ErrorMessage = "Enter a valid year in YYYY format")]
+    public int? Year { get; init; }
 }
