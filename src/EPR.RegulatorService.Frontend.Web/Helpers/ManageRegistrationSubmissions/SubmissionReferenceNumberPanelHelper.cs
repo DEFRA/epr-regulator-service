8415db459517a633a540b1344216bd6f63fe2c92
Shehzad Ismail <shehzad.ismail@atos.net>
2025-06-26 11:12:42 +0100

    [refactor/regulator-service: Normal submission reference panel helper refactored]

---
+++ src/EPR.RegulatorService.Frontend.Web/Helpers/ManageRegistrationSubmissions/SubmissionReferenceNumberPanelHelper.cs
@@ -0,0 +1,15 @@
+using EPR.RegulatorService.Frontend.Web.ViewModels.ManageRegistrationSubmissions;
+
+namespace EPR.RegulatorService.Frontend.Web.Helpers.ManageRegistrationSubmissions
+{
+    public static class SubmissionReferenceNumberPanelHelper
+    {
+        public static string GetPanelTitle(RegistrationSubmissionDetailsViewModel model) => !string.IsNullOrWhiteSpace(model.ProducerRegistrationNumber)
+                ? "RegistrationSubmissionDetails.RegistrationReferenceNumber"
+                : "RegistrationSubmissionDetails.ApplicationReferenceNumber";
+
+        public static string GetPanelContent(RegistrationSubmissionDetailsViewModel model) => !string.IsNullOrWhiteSpace(model.ProducerRegistrationNumber)
+            ? model.ProducerRegistrationNumber
+            : model.ApplicationReferenceNumber;
+    }
+}
---
+++ src/EPR.RegulatorService.Frontend.Web/TagHelpers/ManageRegistrationSubmissions/SubmissionReferenceNumberPanelTagHelper.cs
@@ -0,0 +1,40 @@
+namespace EPR.RegulatorService.Frontend.Web.TagHelpers.ManageRegistrationSubmissions
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    [HtmlTargetElement("submission-reference-number-panel", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class SubmissionReferenceNumberPanelTagHelper : TagHelper
+    {
+        private const string HeadingAttributeName = "heading";
+        private const string ContentAttributeName = "content";
+        private const string StatusAttributeName = "status";
+
+        [HtmlAttributeName(HeadingAttributeName)]
+        public string? Heading { get; set; }
+
+        [HtmlAttributeName(ContentAttributeName)]
+        public string? Content { get; set; }
+
+        [HtmlAttributeName(StatusAttributeName)]
+        public string? Status { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string statusClassName = Status switch
+            {
+                "Granted" => "info-panel__granted",
+                "Refused" => "info-panel__refused",
+                "Pending" => "info-panel__pending",
+                "Cancelled" => "info-panel__cancelled",
+                "Queried" => "info-panel__queried",
+                "Updated" => "info-panel__updated",
+                _ => string.Empty
+            };
+
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("class", $"info-panel {statusClassName}");
+            output.Content.AppendHtml($"<h3>{Heading}</h3><p>{Content}</p>");
+        }
+    }
+}
