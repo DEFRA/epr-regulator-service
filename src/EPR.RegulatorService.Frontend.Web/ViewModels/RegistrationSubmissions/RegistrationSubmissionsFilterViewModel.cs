abc278338481afdd97323e96fd621a1eae838883
Nick Buckland <nicholas.buckland@equalexperts.com>
2025-12-10 16:58:10 +0000

    Added IsDirectProducerChecked property to support the new checkbox, also amended the clearfilter and type conversion operator methods.

--- src/EPR.RegulatorService.Frontend.Web/ViewModels/RegistrationSubmissions/RegistrationSubmissionsFilterViewModel.cs
+++ src/EPR.RegulatorService.Frontend.Web/ViewModels/RegistrationSubmissions/RegistrationSubmissionsFilterViewModel.cs
@@ -1,4 +1,5 @@
 namespace EPR.RegulatorService.Frontend.Web.ViewModels.RegistrationSubmissions;
+
 using EPR.RegulatorService.Frontend.Core.Models.RegistrationSubmissions;
 
 public class RegistrationSubmissionsFilterViewModel
@@ -9,6 +10,7 @@ public class RegistrationSubmissionsFilterViewModel
     public string? OrganisationRef { get; set; } = string.Empty;
 
     public bool IsOrganisationComplianceChecked { get; set; }
+    public bool IsDirectProducerChecked { get; set; }
     public bool IsOrganisationSmallChecked { get; set; }
     public bool IsOrganisationLargeChecked { get; set; }
     public bool IsStatusGrantedChecked { get; set; }
@@ -39,9 +41,12 @@ public class RegistrationSubmissionsFilterViewModel
             if (value)
             {
                 OrganisationName = OrganisationRef = null;
-                IsOrganisationComplianceChecked = IsOrganisationLargeChecked = IsOrganisationSmallChecked = false;
-                IsStatusCancelledChecked = IsStatusGrantedChecked = IsStatusPendingChecked = IsStatusQueriedChecked = IsStatusRefusedChecked = IsStatusUpdatedChecked = false;
-                IsResubmissionPendingRegistrationChecked = IsResubmissionAcceptedRegistrationChecked = IsResubmissionRejectedRegistrationChecked = false;
+                IsOrganisationComplianceChecked = IsDirectProducerChecked =
+                    IsOrganisationLargeChecked = IsOrganisationSmallChecked = false;
+                IsStatusCancelledChecked = IsStatusGrantedChecked = IsStatusPendingChecked =
+                    IsStatusQueriedChecked = IsStatusRefusedChecked = IsStatusUpdatedChecked = false;
+                IsResubmissionPendingRegistrationChecked = IsResubmissionAcceptedRegistrationChecked =
+                    IsResubmissionRejectedRegistrationChecked = false;
                 Is2025Checked = false;
                 Is2026Checked = false;
             }
@@ -51,63 +56,121 @@ public class RegistrationSubmissionsFilterViewModel
     public bool IsFilterApplied { get; set; }
     public int NationId { get; set; }
 
-    public static implicit operator RegistrationSubmissionsFilterModel(RegistrationSubmissionsFilterViewModel viewModel) => new RegistrationSubmissionsFilterModel
+    public static implicit operator
+        RegistrationSubmissionsFilterModel(RegistrationSubmissionsFilterViewModel viewModel) => new()
     {
         PageNumber = viewModel.PageNumber,
         PageSize = viewModel.PageSize,
         NationId = viewModel.NationId,
         Show2026RelevantYearFilter = viewModel.Show2026RelevantYearFilter,
-        RelevantYears = string.Join(", ", new[]
-        {
-            viewModel.Is2025Checked ? "2025" : null,
-            viewModel.Is2026Checked ? "2026" : null
-        }.Where(year => !string.IsNullOrWhiteSpace(year))),
+        RelevantYears =
+            string.Join(", ",
+                new[] { viewModel.Is2025Checked ? "2025" : null, viewModel.Is2026Checked ? "2026" : null }
+                    .Where(year => !string.IsNullOrWhiteSpace(year))),
         OrganisationReference = !string.IsNullOrEmpty(viewModel.OrganisationRef) ? viewModel.OrganisationRef : null,
         OrganisationName = !string.IsNullOrEmpty(viewModel.OrganisationName) ? viewModel.OrganisationName : null,
-        OrganisationType = string.Join(" ", new[]
-                               {
-                                   viewModel.IsOrganisationComplianceChecked ? Core.Enums.RegistrationSubmissionOrganisationType.compliance.ToString() : null,
-                                   viewModel.IsOrganisationLargeChecked ? Core.Enums.RegistrationSubmissionOrganisationType.large.ToString() : null,
-                                   viewModel.IsOrganisationSmallChecked ? Core.Enums.RegistrationSubmissionOrganisationType.small.ToString() : null
-                               }.Where(x => !string.IsNullOrEmpty(x))),
-        Statuses = string.Join(" ", new[]
-                               {
-                                    viewModel.IsStatusCancelledChecked ? Core.Enums.RegistrationSubmissionStatus.Cancelled.ToString() : null,
-                                    viewModel.IsStatusGrantedChecked ? Core.Enums.RegistrationSubmissionStatus.Granted.ToString() : null,
-                                    viewModel.IsStatusPendingChecked ? Core.Enums.RegistrationSubmissionStatus.Pending.ToString() : null,
-                                    viewModel.IsStatusQueriedChecked ? Core.Enums.RegistrationSubmissionStatus.Queried.ToString() : null,
-                                    viewModel.IsStatusRefusedChecked ? Core.Enums.RegistrationSubmissionStatus.Refused.ToString() : null,
-                                    viewModel.IsStatusUpdatedChecked ? Core.Enums.RegistrationSubmissionStatus.Updated.ToString() : null
-                               }.Where(x => !string.IsNullOrEmpty(x))),
-        ResubmissionStatuses = string.Join(" ", new[]
-                               {
-                                    viewModel.IsResubmissionPendingRegistrationChecked ? Core.Enums.RegistrationSubmissionStatus.Pending.ToString() : null,
-                                    viewModel.IsResubmissionAcceptedRegistrationChecked ? Core.Enums.RegistrationSubmissionStatus.Accepted.ToString() : null,
-                                    viewModel.IsResubmissionRejectedRegistrationChecked ? Core.Enums.RegistrationSubmissionStatus.Rejected.ToString() : null
-                               }.Where(x => !string.IsNullOrEmpty(x)))
+        OrganisationType =
+            string.Join(" ",
+                new[]
+                {
+                    viewModel.IsOrganisationComplianceChecked
+                        ? Core.Enums.RegistrationSubmissionOrganisationType.compliance.ToString()
+                        : null,
+                    viewModel.IsDirectProducerChecked
+                        ? Core.Enums.RegistrationSubmissionOrganisationType.direct.ToString()
+                        : null,
+                    viewModel.IsOrganisationLargeChecked
+                        ? Core.Enums.RegistrationSubmissionOrganisationType.large.ToString()
+                        : null,
+                    viewModel.IsOrganisationSmallChecked
+                        ? Core.Enums.RegistrationSubmissionOrganisationType.small.ToString()
+                        : null
+                }.Where(x => !string.IsNullOrEmpty(x))),
+        Statuses =
+            string.Join(" ",
+                new[]
+                {
+                    viewModel.IsStatusCancelledChecked
+                        ? Core.Enums.RegistrationSubmissionStatus.Cancelled.ToString()
+                        : null,
+                    viewModel.IsStatusGrantedChecked
+                        ? Core.Enums.RegistrationSubmissionStatus.Granted.ToString()
+                        : null,
+                    viewModel.IsStatusPendingChecked
+                        ? Core.Enums.RegistrationSubmissionStatus.Pending.ToString()
+                        : null,
+                    viewModel.IsStatusQueriedChecked
+                        ? Core.Enums.RegistrationSubmissionStatus.Queried.ToString()
+                        : null,
+                    viewModel.IsStatusRefusedChecked
+                        ? Core.Enums.RegistrationSubmissionStatus.Refused.ToString()
+                        : null,
+                    viewModel.IsStatusUpdatedChecked
+                        ? Core.Enums.RegistrationSubmissionStatus.Updated.ToString()
+                        : null
+                }.Where(x => !string.IsNullOrEmpty(x))),
+        ResubmissionStatuses = string.Join(" ",
+            new[]
+            {
+                viewModel.IsResubmissionPendingRegistrationChecked
+                    ? Core.Enums.RegistrationSubmissionStatus.Pending.ToString()
+                    : null,
+                viewModel.IsResubmissionAcceptedRegistrationChecked
+                    ? Core.Enums.RegistrationSubmissionStatus.Accepted.ToString()
+                    : null,
+                viewModel.IsResubmissionRejectedRegistrationChecked
+                    ? Core.Enums.RegistrationSubmissionStatus.Rejected.ToString()
+                    : null
+            }.Where(x => !string.IsNullOrEmpty(x)))
     };
 
-    public static implicit operator RegistrationSubmissionsFilterViewModel(RegistrationSubmissionsFilterModel model) => new RegistrationSubmissionsFilterViewModel
-    {
-        OrganisationName = model.OrganisationName,
-        OrganisationRef = model.OrganisationReference,
-        IsOrganisationComplianceChecked = model.OrganisationType != null && model.OrganisationType.Contains("compliance", StringComparison.OrdinalIgnoreCase),
-        IsOrganisationSmallChecked = model.OrganisationType != null && model.OrganisationType.Contains("small", StringComparison.OrdinalIgnoreCase),
-        IsOrganisationLargeChecked = model.OrganisationType != null && model.OrganisationType.Contains("large", StringComparison.OrdinalIgnoreCase),
-        IsStatusGrantedChecked = model.Statuses != null && model.Statuses.Contains("granted", StringComparison.OrdinalIgnoreCase),
-        IsStatusRefusedChecked = model.Statuses != null && model.Statuses.Contains("refused", StringComparison.OrdinalIgnoreCase),
-        IsStatusPendingChecked = model.Statuses != null && model.Statuses.Contains("pending", StringComparison.OrdinalIgnoreCase),
-        IsStatusQueriedChecked = model.Statuses != null && model.Statuses.Contains("queried", StringComparison.OrdinalIgnoreCase),
-        IsStatusUpdatedChecked = model.Statuses != null && model.Statuses.Contains("updated", StringComparison.OrdinalIgnoreCase),
-        IsStatusCancelledChecked = model.Statuses != null && model.Statuses.Contains("cancelled", StringComparison.OrdinalIgnoreCase),
-        IsResubmissionPendingRegistrationChecked = model.ResubmissionStatuses != null && model.ResubmissionStatuses.Contains("pending", StringComparison.OrdinalIgnoreCase),
-        IsResubmissionAcceptedRegistrationChecked = model.ResubmissionStatuses != null && model.ResubmissionStatuses.Contains("accepted", StringComparison.OrdinalIgnoreCase),
-        IsResubmissionRejectedRegistrationChecked = model.ResubmissionStatuses != null && model.ResubmissionStatuses.Contains("rejected", StringComparison.OrdinalIgnoreCase),
-        Is2025Checked = model.RelevantYears != null && model.RelevantYears.Contains("2025", StringComparison.OrdinalIgnoreCase),
-        Is2026Checked = model.RelevantYears != null && model.RelevantYears.Contains("2026", StringComparison.OrdinalIgnoreCase),
-        PageNumber = model.PageNumber ?? 1,
-        PageSize = model.PageSize ?? 20,
-        NationId = model.NationId,
-        Show2026RelevantYearFilter = model.Show2026RelevantYearFilter
-    };
+    public static implicit operator RegistrationSubmissionsFilterViewModel(RegistrationSubmissionsFilterModel model) =>
+        new()
+        {
+            OrganisationName = model.OrganisationName,
+            OrganisationRef = model.OrganisationReference,
+            IsOrganisationComplianceChecked =
+                model.OrganisationType != null &&
+                model.OrganisationType.Contains("compliance", StringComparison.OrdinalIgnoreCase),
+            IsDirectProducerChecked =
+                model.OrganisationType != null &&
+                model.OrganisationType.Contains("direct", StringComparison.OrdinalIgnoreCase),
+            IsOrganisationSmallChecked =
+                model.OrganisationType != null &&
+                model.OrganisationType.Contains("small", StringComparison.OrdinalIgnoreCase),
+            IsOrganisationLargeChecked =
+                model.OrganisationType != null &&
+                model.OrganisationType.Contains("large", StringComparison.OrdinalIgnoreCase),
+            IsStatusGrantedChecked =
+                model.Statuses != null && model.Statuses.Contains("granted", StringComparison.OrdinalIgnoreCase),
+            IsStatusRefusedChecked =
+                model.Statuses != null && model.Statuses.Contains("refused", StringComparison.OrdinalIgnoreCase),
+            IsStatusPendingChecked =
+                model.Statuses != null && model.Statuses.Contains("pending", StringComparison.OrdinalIgnoreCase),
+            IsStatusQueriedChecked =
+                model.Statuses != null && model.Statuses.Contains("queried", StringComparison.OrdinalIgnoreCase),
+            IsStatusUpdatedChecked =
+                model.Statuses != null && model.Statuses.Contains("updated", StringComparison.OrdinalIgnoreCase),
+            IsStatusCancelledChecked =
+                model.Statuses != null && model.Statuses.Contains("cancelled", StringComparison.OrdinalIgnoreCase),
+            IsResubmissionPendingRegistrationChecked =
+                model.ResubmissionStatuses != null &&
+                model.ResubmissionStatuses.Contains("pending", StringComparison.OrdinalIgnoreCase),
+            IsResubmissionAcceptedRegistrationChecked =
+                model.ResubmissionStatuses != null &&
+                model.ResubmissionStatuses.Contains("accepted", StringComparison.OrdinalIgnoreCase),
+            IsResubmissionRejectedRegistrationChecked =
+                model.ResubmissionStatuses != null &&
+                model.ResubmissionStatuses.Contains("rejected", StringComparison.OrdinalIgnoreCase),
+            Is2025Checked =
+                model.RelevantYears != null &&
+                model.RelevantYears.Contains("2025", StringComparison.OrdinalIgnoreCase),
+            Is2026Checked =
+                model.RelevantYears != null &&
+                model.RelevantYears.Contains("2026", StringComparison.OrdinalIgnoreCase),
+            PageNumber = model.PageNumber ?? 1,
+            PageSize = model.PageSize ?? 20,
+            NationId = model.NationId,
+            Show2026RelevantYearFilter = model.Show2026RelevantYearFilter
+        };
 }
