9bde9518e8f7e9a60f9a50cc4233ef359de1447b
Tim Abell <t@0x5.uk>
2026-01-16 11:30:46 +0000

    Redirect `/` client-side to `/regulators/home`
    This means we get a 200-ok response to keep the kudu healthcheck happy
    (doesn't seem to like 302 responses), and a nice local dev experience
    redirecting to the path base
    
    The root path '/' is not exposed to users in production or dev
    environments as there is path based routing of a proxy thing and only
    /regulators comes to this microservice
    
    Co-Authored-By: Paul Barnard <paul.barnard@esynergy.co.uk>

--- src/EPR.RegulatorService.Frontend.Web/Program.cs
+++ src/EPR.RegulatorService.Frontend.Web/Program.cs
@@ -73,11 +73,25 @@ builder.WebHost.ConfigureKestrel(options => options.AddServerHeader = false);
 
 var app = builder.Build();
 
+// Redirect `/` to the homepage
+// Using a client-side redirect so that kudu healthcheck still receives 200-ok instead of 302
 app.Use(async (context, next) =>
 {
+    string redirectTo = $"{builder.Configuration.GetValue<string>("PATH_BASE")}/home";
     if (context.Request.Path == "/" && context.Request.PathBase == PathString.Empty)
     {
-        context.Response.StatusCode = StatusCodes.Status204NoContent;
+        context.Response.StatusCode = StatusCodes.Status200OK;
+        context.Response.ContentType = "text/html";
+        await context.Response.WriteAsync($"""
+            <!DOCTYPE html>
+            <html>
+            <head><title>Redirecting...</title></head>
+            <body>
+                <p>Redirecting to <a href="{redirectTo}">{redirectTo}</a></p>
+                <script>window.location.href = '{redirectTo}';</script>
+            </body>
+            </html>
+            """);
         return;
     }
 
