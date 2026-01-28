8fc3b1f2be08a2a2b85138973e92432ec73a01a0
Tim Abell <t@0x5.uk>
2026-01-28 09:39:41 +0000

    Write preview release notes to file

--- generate-release-notes-preview.sh
+++ generate-release-notes-preview.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # requires https://git-cliff.org/
 # show what would be in this release if we cut a release now and tagged it
-git-cliff --unreleased --strip header --config cliff.toml
+
+echo 'git-cliff --unreleased --strip header --config cliff.toml' > release-note-preview.txt
+
+git-cliff --unreleased --strip header --config cliff.toml | tee release-note-preview.txt
