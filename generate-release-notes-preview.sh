a3bdee067aee671f89ae217cdc562493f32d8f7f
Tim Abell <t@0x5.uk>
2026-01-28 10:33:19 +0000

    Output preview has md file
    For easier preview

--- generate-release-notes-preview.sh
+++ generate-release-notes-preview.sh
@@ -2,6 +2,6 @@
 # requires https://git-cliff.org/
 # show what would be in this release if we cut a release now and tagged it
 
-echo 'git-cliff --unreleased --strip header --config cliff.toml' > release-note-preview.txt
+echo 'git-cliff --unreleased --strip header --config cliff.toml' > release-note-preview.md
 
-git-cliff --unreleased --strip header --config cliff.toml | tee release-note-preview.txt
+git-cliff --unreleased --strip header --config cliff.toml | tee release-note-preview.md
