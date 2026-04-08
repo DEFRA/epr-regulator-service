c2922523aa6930087f810f9e42de1f917df956b1
Daniel Ashton <daniel.ashton@dashkat.uk>
2026-04-08 13:01:08 +0100

    AMCR-65 - Revert nullable change on fileid

--- src/EPR.RegulatorService.Frontend.Core/Models/Submissions/Submission.cs
+++ src/EPR.RegulatorService.Frontend.Core/Models/Submissions/Submission.cs
@@ -3,7 +3,7 @@ namespace EPR.RegulatorService.Frontend.Core.Models.Submissions;
 public class Submission : AbstractSubmission
 {
     public bool IsResubmissionRequired { get; set; }
-    public Guid? FileId { get; set; }
+    public Guid FileId { get; set; }
     public string? ProducerType { get; set; }
     public Guid? ComplianceSchemeId { get; set; }
     public string? ActualSubmissionPeriod { get; set; }
