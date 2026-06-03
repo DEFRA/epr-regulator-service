5e1a586fce4f792a55fef4051015e07a0600807f
Nick Buckland <nicholas.buckland@equalexperts.com>
2026-06-03 15:08:50 +0100

    SonarQube: Remove the unused local variable 'result'

--- src/EPR.RegulatorService.Frontend.Core/Services/PaymentFacadeService.cs
+++ src/EPR.RegulatorService.Frontend.Core/Services/PaymentFacadeService.cs
@@ -69,7 +69,6 @@ public class PaymentFacadeService : IPaymentFacadeService
 
         var response = await _httpClient.PostAsJsonAsync(_paymentFacadeApiConfig.Endpoints[GetProducerPaymentDetailsPath], request);
         response.EnsureSuccessStatusCode();
-        var result = response.Content.ReadAsStringAsync();
 
         return JsonSerializer.Deserialize<ProducerPaymentResponse>(await response.Content.ReadAsStringAsync());
     }
