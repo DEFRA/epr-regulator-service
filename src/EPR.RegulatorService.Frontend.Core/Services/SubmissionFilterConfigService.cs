72cf2174d515bfa5a01b34e61dfd337e5d5f0598
Daniel Ashton <daniel.ashton@dashkat.uk>
2026-07-08 17:15:55 +0100

    AMCR-321 AMCR-322
    Covers dynamic adding of year filters based on date. This still partially relies on the existing config then will add the new periods and years as required.

--- src/EPR.RegulatorService.Frontend.Core/Services/SubmissionFilterConfigService.cs
+++ src/EPR.RegulatorService.Frontend.Core/Services/SubmissionFilterConfigService.cs
@@ -8,10 +8,13 @@ namespace EPR.RegulatorService.Frontend.Core.Services
 
     public class SubmissionFilterConfigService(
         IOptions<SubmissionFiltersConfig> submissionFiltersConfig,
-        IOptions<PackagingDataSubmissionsConfig> packagingDataSubmissionsConfig) : ISubmissionFilterConfigService
+        IOptions<PackagingDataSubmissionsConfig> packagingDataSubmissionsConfig,
+        TimeProvider timeProvider) : ISubmissionFilterConfigService
     {
         public (int[] Years, string[] Periods) GetFilteredSubmissionYearsAndPeriods()
         {
+
+
             var submissionFiltersOptions = submissionFiltersConfig.Value;
             var packagingDataSubmissionOptions = packagingDataSubmissionsConfig.Value;
 
@@ -33,6 +36,40 @@ namespace EPR.RegulatorService.Frontend.Core.Services
             string[] filteredPeriods = submissionPeriods
                 .Where(sp => IsBetween(int.Parse(sp[^4..], CultureInfo.InvariantCulture), startingYear, latestYear))
                 .ToArray();
+
+            var startingYear2 = 2026;
+            var window1 = "January to June ";
+            var window2 = "July to December ";
+            var window3 = "January to December ";
+            if (timeProvider.GetUtcNow().Year >= startingYear2)
+            {
+
+                var addYearData = true;
+                while (addYearData)
+                {
+                    if (timeProvider.GetUtcNow().Year == startingYear2)
+                    {
+                        if (timeProvider.GetUtcNow().Month > 6)
+                        {
+                            filteredPeriods = filteredPeriods.Append(window1 + startingYear2).ToArray();
+                            filteredYears = filteredYears.Append(startingYear2).ToArray();
+                        }
+                        addYearData = false;
+                    }
+
+                    if (timeProvider.GetUtcNow().Year > startingYear2)
+                    {
+                        filteredPeriods = filteredPeriods
+                            .Append(window1 + startingYear2)
+                            .Append(window2 + startingYear2)
+                            .Append(window3 + startingYear2)
+                            .ToArray();
+                        filteredYears = filteredYears.Append(startingYear2).ToArray();
+                    }
+
+                    startingYear2++;
+                }
+            }
 
             return (filteredYears, filteredPeriods);
         }
